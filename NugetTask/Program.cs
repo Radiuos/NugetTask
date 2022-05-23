4692af836b5239a6c37328ba1cbb4b005f4bd622
Radiuos <80468351+Radiuos@users.noreply.github.com>
2022-05-23 13:11:10 +0300

    Update Program.cs

--- NugetTask/Program.cs
+++ NugetTask/Program.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using Humanizer;
+using System;
+//using Humanizer;
 namespace NugetTask
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(TimeSpan.FromMilliseconds(3603001).Humanize(3));
+            //Console.WriteLine(TimeSpan.FromMilliseconds(3603001).Humanize(3));
+            Console.WriteLine(TimeSpan.FromMilliseconds(3603001));
         }
     }
 }
